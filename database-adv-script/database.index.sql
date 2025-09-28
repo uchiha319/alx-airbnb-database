442a520ab8da6112d94ae984ac438039e8c960d9
uchiha319 <143499201+uchiha319@users.noreply.github.com>
2025-09-28 19:04:21 +0300

    Create database.index.sql

---
+++ database-adv-script/database.index.sql
@@ -0,0 +1,21 @@
+EXPLAIN ANALYZE SELECT * 
+FROM properties 
+JOIN reviews ON properties.property_id = reviews.property_id
+WHERE properties.property_id IN (
+    SELECT property_id 
+    FROM reviews 
+    GROUP BY property_id 
+    HAVING AVG(rating) > 4.0
+);
+
+CREATE INDEX reviews_property_idx ON reviews(property_id);
+
+EXPLAIN ANALYZE SELECT * 
+FROM properties 
+JOIN reviews ON properties.property_id = reviews.property_id
+WHERE properties.property_id IN (
+    SELECT property_id 
+    FROM reviews 
+    GROUP BY property_id 
+    HAVING AVG(rating) > 4.0
+);
